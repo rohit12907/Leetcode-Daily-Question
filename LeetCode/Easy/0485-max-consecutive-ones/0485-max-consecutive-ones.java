b12614a275b1deac451fc040366c2f871379d55f
Rohit Rawat <rawatrohit0732@gmail.com>
2026-03-21 23:25:48 +0530

    3 ms (61.79%) 03-21-2026 52.2 MB (94.34%) 0485-max-consecutive-ones Easy .java Array

---
+++ LeetCode/Easy/0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max1 = 0 ;
+        int  count = 0;
+        for(int i = 0 ;nums.length>i;i++){
+        //    int  count = 1;
+            if(nums[i]==1){
+                count++;
+                max1 = Math.max(max1,count );
+            }
+            else{
+                count = 0 ;
+                // continue;
+                //  count = 0 ;
+            }
+        }
+        return max1;
+        
+    }
+}
