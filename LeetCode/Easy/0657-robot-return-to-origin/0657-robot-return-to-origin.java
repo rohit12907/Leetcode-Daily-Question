36dedf129f02a7c1c01b26767f84db76d3f8c034
Rohit Rawat <rawatrohit0732@gmail.com>
2026-04-06 00:01:12 +0530

    7 ms (30.8%) 04-06-2026 45.2 MB (98.05%) 0657-robot-return-to-origin Easy .java String

--- LeetCode/Easy/0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ LeetCode/Easy/0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,12 +1,30 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int x = 0, y = 0;
-        for (char move: moves.toCharArray()) {
-            if (move == 'U') y--;
-            else if (move == 'D') y++;
-            else if (move == 'L') x--;
-            else if (move == 'R') x++;
+        int l = moves.length();
+        int jump = 0;
+        int R = 0;
+        int L = 0 ;
+        int D = 0 ;
+        int U = 0 ;
+        for(int i =0 ;l>i;i++){
+            if(moves.charAt(i)==('U')){
+                U++;
+            }
+            else if(moves.charAt(i)== ('D')){
+                D--;
+            }
+              else if(moves.charAt(i)== ('R')){
+                R++;
+            }
+              else {
+                L--;
+            }
         }
-        return x == 0 && y == 0;
+        if(R+L==0 && D + U ==0){
+            return true;
+        }
+        else{
+           return false;
+        }
     }
 }
