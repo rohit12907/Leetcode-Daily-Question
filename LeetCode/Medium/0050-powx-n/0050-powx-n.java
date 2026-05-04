2feb80e74327e8a3393e91c3e4ced985793cf861
Rohit Rawat <rawatrohit0732@gmail.com>
2026-05-04 13:48:42 +0530

    0 ms (100%) 05-04-2026 47.9 MB (64.44%) 0050-powx-n Medium .java Math

--- LeetCode/Medium/0050-powx-n/0050-powx-n.java
+++ LeetCode/Medium/0050-powx-n/0050-powx-n.java
@@ -1,19 +1,6 @@
 class Solution {
     public double myPow(double x, int n) {
-        long N = n; // handle overflow
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
-
-        double result = 1.0;
-        while (N > 0) {
-            if (N % 2 == 1) { // odd power
-                result *= x;
-            }
-            x *= x; // square the base
-            N /= 2; // halve the exponent
-        }
-        return result;
+   return Math.pow(x,n);
+   
     }
 }
