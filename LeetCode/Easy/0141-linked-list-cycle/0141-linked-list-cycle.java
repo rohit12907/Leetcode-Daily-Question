198c8c2422f4974641c2a85a5d43d72352a24de7
Rohit Rawat <rawatrohit0732@gmail.com>
2026-05-19 16:01:23 +0530

    0 ms (100%) 05-19-2026 46.5 MB (85.17%) 0141-linked-list-cycle Easy .java Hash Table

--- LeetCode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.java
+++ LeetCode/Easy/0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,13 +11,19 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode s = head ;
-        ListNode f = head ;
-        while(f!=null && f.next != null){
-            s = s.next ;
-            f = f.next.next;
-            if(f==s) return true;
+ListNode  s = head ;
+      ListNode  f = head ;
+        while((f != null )&&(f.next != null)){
+             f = f.next.next ;
+            s= s.next ;
+
+    //    /  /   f = f.next.next ;
+            if(f==s){
+                return true;
+            }
+       
+
         }
-        return false ;
+return false ;
     }
 }
