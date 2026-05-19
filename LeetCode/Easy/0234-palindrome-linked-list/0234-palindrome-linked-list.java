374162d1b56cfd949eaea63ddb4e2cee0db592a2
Rohit Rawat <rawatrohit0732@gmail.com>
2026-05-19 13:34:29 +0530

    7 ms (31.07%) 05-19-2026 99.8 MB (16.52%) 0234-palindrome-linked-list Easy .java Linked List

---
+++ LeetCode/Easy/0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode temp = head ;
+         ArrayList<Integer> list = new ArrayList<>();
+         while(temp != null){
+            list.add(temp.val);
+            temp = temp.next;
+         }
+         int i = 0 ;
+         int j = list.size()-1 ;
+             while(i < j){
+
+            if(!list.get(i).equals(list.get(j))){
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
