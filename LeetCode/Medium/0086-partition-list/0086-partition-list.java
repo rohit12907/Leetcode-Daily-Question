4c66370c61a6d26c13b6447d63fce86044ffc653
Rohit Rawat <rawatrohit0732@gmail.com>
2026-05-26 15:51:00 +0530

    0 ms (100%) 05-26-2026 44 MB (13.93%) 0086-partition-list Medium .java Linked List

---
+++ LeetCode/Medium/0086-partition-list/0086-partition-list.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        if(head==null || head.next ==null) return head ; 
+        ListNode d1= new ListNode(-1);
+        ListNode d2 = new ListNode(-1);
+        ListNode t1 = d1 ; 
+        ListNode t2 = d2 ; 
+        ListNode t = head ; 
+        while(t!= null){
+           if(t.val>=x){
+            t2.next = t ;
+            t2 = t ;
+           
+           }
+           else{
+            t1.next = t ;
+            t1 = t ; 
+          
+           }
+           t = t.next ;
+
+        }
+        t1.next = d2.next ;
+        t2.next = null;
+ 
+        return d1.next ;
+
+        
+    }
+}
