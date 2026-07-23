4461d9a831dcace7455b0ae298403f3f16137887
Rohit Rawat <rawatrohit0732@gmail.com>
2026-07-23 19:32:40 +0530

    0 ms (100%) 07-23-2026 125.4 MB (26.53%) 3513-number-of-unique-xor-triplets-i Medium .java Array

---
+++ LeetCode/Medium/3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+
+        int n = nums.length;
+
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int bits = 32 - Integer.numberOfLeadingZeros(n);
+
+        return 1 << bits;
+    }
+}
